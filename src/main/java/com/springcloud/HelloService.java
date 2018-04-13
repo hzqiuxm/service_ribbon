48163fb59a11b75750554ce2a1825cbc67602163
qiuxm <qiuxm@yunhetong.net>
2018-04-13 18:07:03 +0800

    增加断路器容错保护

--- src/main/java/com/springcloud/HelloService.java
+++ src/main/java/com/springcloud/HelloService.java
@@ -1,5 +1,6 @@
 package com.springcloud;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -23,7 +24,20 @@ public class HelloService {
      * SERVICE-HI ：虚拟主机名
      * restTemplate+Ribbon 可以应对简单的URL 如果url复杂，使用Feign,它整合了Ribbon
      */
+
+    @HystrixCommand(fallbackMethod = "hiFallback")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+
+    /**
+     * 降级服务处理
+     * 加入Hysrix使用回调方法，使用注解方式时必须和调用服务在一个类中
+     * @return
+     */
+    public String hiFallback(String name){
+
+        return "hi service has a error!" + name;
     }
 }
